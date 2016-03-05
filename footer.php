cb2d3cb493656f3357ed73ac4beda981f766cd42
marioernestoms <marioernesto.ms@gmail.com>
2016-03-05 10:41:05 -0300

    mudanças nos diretórios do tema

--- footer.php
+++ footer.php
@@ -16,8 +16,6 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'marioernestoms' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'marioernestoms' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'marioernestoms' ), 'marioernestoms', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
